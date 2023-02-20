3a40023dbe3fe33bb8b3656c60221c560770d322
Subhashini Anaivarathan <117558114+SUBHASHINI-A-DEVELOPE@users.noreply.github.com>
2023-02-19 20:28:58 -0800

    Add files via upload

---
+++ Tkinter @Chatbot.py
@@ -0,0 +1,31 @@
+
+from tkinter import *
+root = Tk()
+root.configure(bg='black')
+root.title("Chatbot@Subha")
+
+def send():
+    send = "You: "+e.get()
+    txt.insert(END, "|n"+send)
+    user = e.get().lower()
+    if(user == "hello"):
+        txt.insert(END, "|n" + "Bot: Hi")
+    elif(user == "hi" or user == "hii" or user == "hiiii"):
+        txt.insert(END, "|n" + "Bot: Hello")
+    elif(e.get() == "how are you"):
+        txt.insert(END, "|n" + "Bot: fine! and you")
+    elif(user == "fine" or user == "i am good" or user == "i am doing good"):
+        txt.insert(END, "|n" + "Bot: Great! how can I help you.")
+    elif(user == "hi" or user == "ok" or user == "okay"):
+        txt.insert(END, "|n" + "Bot: great")
+    else:
+        txt.insert(END, "|n" + "Bot: Sorry! I dind't got you")
+    e.delete(0, END)
+
+txt = Text(root)
+txt.grid(row=0, column=0, columnspan=2)
+e = Entry(root, width=100)
+e.grid(row=1, column=0)
+
+send = Button(root, text="Send", command=send).grid(row=1, column=1)
+root.mainloop()
